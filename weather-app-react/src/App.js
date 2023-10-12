e3a96fcc46c65dff91355ba8f4921bd7eba41f86
joanclarke <jcjuju2014@gmail.com>
2023-10-12 19:16:22 -0400

    add API key

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+
 function App() {
-  return <div className="App"></div>
+  // const url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=dallas&appid=4c42be5a817769952f07dc6e780087c7'
+
+  return <div className="App">HELLOOOOOO THANK GOD</div>
 }
 
 export default App
