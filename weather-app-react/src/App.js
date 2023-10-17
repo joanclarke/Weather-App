fb18d2b7e8c0ffcea05e9f5a0172bf349605d96b
joanclarke <jcjuju2014@gmail.com>
2023-10-16 23:14:13 -0400

    edit info

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4c42be5a817769952f07dc6e780087c7`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=4c42be5a817769952f07dc6e780087c7`
 
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
@@ -36,18 +36,18 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
-        {data.name != undefined && (
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
               {data.main ? (
-                <p className="bold">{data.main.feels_like}°F</p>
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
               ) : null}
               <p>Feels like</p>
             </div>
@@ -56,7 +56,9 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed}MPH</p> : null}
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
               <p>Wind Speed</p>
             </div>
           </div>
